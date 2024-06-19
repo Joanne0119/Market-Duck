67af18e976d225744424eaf711741d5f41d09539
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-06-19 21:52:43 +0800

    Initial Commit

---
+++ Market Duck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Market Duck
+//
+//  Created by 劉丞恩 on 2024/6/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Market Duck/Market_DuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Market_DuckApp.swift
+//  Market Duck
+//
+//  Created by 劉丞恩 on 2024/6/19.
+//
+
+import SwiftUI
+
+@main
+struct Market_DuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
