7d1de4a82d90cf48e361b3f54db901dfb4f9e05f
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-06-20 00:56:13 +0800

    add tool bar

---
+++ Market Duck/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Market Duck
+//
+//  Created by 劉丞恩 on 2024/6/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack{
+            
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
