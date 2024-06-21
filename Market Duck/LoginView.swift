d6e167f6b6239c26016c24699438a3b8a488fafb
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-06-22 00:43:27 +0800

    create signup page

--- Market Duck/LoginView.swift
+++ Market Duck/LoginView.swift
@@ -14,126 +14,130 @@ struct LoginView: View {
     @FocusState var isEmailFocus: Bool
     @FocusState var isPasswordFocus: Bool
     var body: some View {
-        ZStack(alignment:.top){
-            Color("background_color")
-                .ignoresSafeArea()
-            VStack(alignment:.center){
-                ZStack {
-                    Circle()
-                        .frame(width: 140)
-                        .foregroundStyle(.white)
-                    Image("app_icon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200)
-                }
-                .padding(.top, 50)
-                
-                VStack{
-                    TextField(
-                        "電子郵件",
-                        text: $account,
-                        axis: .vertical
-                    )
-                    .focused($isEmailFocus)
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundStyle(isEmailFocus ? .black : Color("trending_block_color"))
-                        .padding(.bottom, 40)
-                
-                    HStack{
-                        if isSecured {
-                            SecureField("密碼", text: $password)
-                                .focused($isPasswordFocus)
-                        } else {
-                            TextField("密碼", text: $password)
-                                .focused($isPasswordFocus)
+        NavigationStack {
+            ZStack(alignment:.top){
+                Color("background_color")
+                    .ignoresSafeArea()
+                VStack(alignment:.center){
+                    ZStack {
+                        Circle()
+                            .frame(width: 140)
+                            .foregroundStyle(.white)
+                        Image("app_icon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200)
+                    }
+                    .padding(.top, 50)
+                    
+                    VStack{
+                        TextField(
+                            "電子郵件",
+                            text: $account,
+                            axis: .vertical
+                        )
+                        .focused($isEmailFocus)
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundStyle(isEmailFocus ? .black : Color("trending_block_color"))
+                            .padding(.bottom, 40)
+                    
+                        HStack{
+                            if isSecured {
+                                SecureField("密碼", text: $password)
+                                    .focused($isPasswordFocus)
+                            } else {
+                                TextField("密碼", text: $password)
+                                    .focused($isPasswordFocus)
+                            }
+                            
+                            Button(action: {
+                                isSecured.toggle()
+                            }) {
+                                Image(systemName: self.isSecured ? "eye.slash" : "eye")
+                                    .accentColor(Color("line_color"))
+                            }
+                        }
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundStyle(isPasswordFocus ? .black : Color("trending_block_color"))
+                        HStack {
+                            Spacer()
+                            Button{
+                                
+                            }label: {
+                                Text("忘記密碼?")
+                                    .font(.system(size: 13))
+                                    .foregroundStyle(Color("line_color"))
+                            }
+                            .padding(.vertical, 8)
                         }
                         
-                        Button(action: {
-                            isSecured.toggle()
-                        }) {
-                            Image(systemName: self.isSecured ? "eye.slash" : "eye")
-                                .accentColor(Color("line_color"))
-                        }
-                    }
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundStyle(isPasswordFocus ? .black : Color("trending_block_color"))
-                    HStack {
-                        Spacer()
-                        Button{
+                        NavigationLink{
                             
                         }label: {
-                            Text("忘記密碼?")
-                                .font(.system(size: 13))
-                                .foregroundStyle(Color("line_color"))
+                            Text("登入")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 19, weight: .regular))
+                                .frame(width: 262, height: 34)
+                                .background(Color("label_color"))
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
                         }
-                        .padding(.vertical, 8)
+                        .padding(.vertical, 5)
+                        NavigationLink{
+                            SignupView()
+                        }label: {
+                            Text("註冊")
+                                .foregroundStyle(Color("line_color"))
+                                .font(.system(size: 19, weight: .regular))
+                                .frame(width: 262, height: 34)
+                                .overlay{
+                                    RoundedRectangle(cornerRadius: 8)
+                                                    .stroke(Color("line_color"), lineWidth: 1)
+                                }
+                        }
+                        .padding(.vertical, 5)
                     }
+                    .padding(.horizontal, 50)
+                    .frame(height: 280)
                     
-                    NavigationLink{
-                        
-                    }label: {
-                        Text("登入")
-                            .foregroundStyle(.white)
-                            .font(.system(size: 19, weight: .regular))
-                            .frame(width: 262, height: 34)
-                            .background(Color("label_color"))
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                    }
-                    .padding(.vertical, 5)
-                    NavigationLink{
-                        
-                    }label: {
-                        Text("註冊")
-                            .foregroundStyle(Color("line_color"))
-                            .font(.system(size: 19, weight: .regular))
-                            .frame(width: 262, height: 34)
-                            .overlay{
-                                RoundedRectangle(cornerRadius: 8)
-                                                .stroke(Color("line_color"), lineWidth: 1)
-                            }
-                    }
-                    .padding(.vertical, 5)
-                }
-                .padding(.horizontal, 50)
-                .frame(height: 280)
-                
-                Rectangle()
-                    .frame(height: 1)
-                    .foregroundStyle(Color("line_color"))
-                    .overlay{
-                        Text("或")
-                            .padding(.horizontal, 6)
-                            .background(Color("background_color"))
-                        
-                    }
-                    .padding(.top, 30)
-                HStack{
-                    Image("fb_icon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width:40)
-                        .padding(.horizontal, 8)
-                    Image("google_icon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width:40)
-                        .padding(.horizontal, 8)
-                }
-                .padding(30)
-                Button{
-                    
-                }label: {
-                    Text("訪客登入")
+                    Rectangle()
+                        .frame(height: 1)
                         .foregroundStyle(Color("line_color"))
-                        .underline()
+                        .overlay{
+                            Text("或")
+                                .padding(.horizontal, 6)
+                                .background(Color("background_color"))
+                            
+                        }
+                        .padding(.top, 30)
+                    HStack{
+                        Image("fb_icon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:40)
+                            .padding(.horizontal, 8)
+                        Image("google_icon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:40)
+                            .padding(.horizontal, 8)
+                    }
+                    .padding(30)
+                    Button{
+                        
+                    }label: {
+                        Text("訪客登入")
+                            .foregroundStyle(Color("line_color"))
+                            .underline()
+                    }
+                    Spacer()
                 }
-                Spacer()
-            }
-            
+                
 
+            }
+            .navigationTitle("登入")
+            .navigationBarHidden(true)
         }
     }
 }
---
+++ Market Duck/SignupView.swift
@@ -0,0 +1,243 @@
+//
+//  SignupView.swift
+//  Market Duck
+//
+//  Created by 劉丞恩 on 2024/6/21.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    @State private var account: String = ""
+    @State private var password: String = ""
+    @State private var name: String = ""
+    @State private var gender: Bool = true //true->boy, false->girl
+    @State private var birthday = Date()
+    @State private var isSecured: Bool = false
+    @FocusState var isEmailFocus: Bool
+    @FocusState var isPasswordFocus: Bool
+    @FocusState var isNameFocus: Bool
+    @State var isGenderFocus: Bool = false
+    @State private var isSelectedGender: Bool = false
+
+    var body: some View {
+        ZStack(alignment:.top){
+            Color("background_color")
+                .ignoresSafeArea()
+            VStack(alignment:.center){
+                ZStack {
+                    Circle()
+                        .frame(width: 140)
+                        .foregroundStyle(.white)
+                    Image("app_icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                }
+                VStack{
+                    HStack{
+                        Text("姓名")
+                            .foregroundStyle(Color("line_color"))
+                            .font(.system(size: 18))
+                        VStack{
+                            TextField(
+                                "姓名",
+                                text: $name,
+                                axis: .vertical
+                            )
+                            .focused($isNameFocus)
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(isNameFocus ? .black : Color("trending_block_color"))
+                                
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    Spacer()
+                    HStack{
+                        Text("性別")
+                            .foregroundStyle(Color("line_color"))
+                            .font(.system(size: 18))
+                        VStack{
+                            Button{
+                                isSelectedGender = true
+                            }label: {
+                                HStack{
+                                    Text(gender ? "男生" : "女生")
+                                        .foregroundStyle(.black)
+                                    Spacer()
+                                    Image(systemName: "chevron.down")
+                                        .foregroundStyle(.black)
+                                }
+                                
+                            }
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(isGenderFocus ? .black : Color("trending_block_color"))
+                                
+                        }
+                        .padding(.leading)
+                    }
+                    Spacer()
+                    
+                    HStack{
+                        Text("生日")
+                            .foregroundStyle(Color("line_color"))
+                            .font(.system(size: 18))
+                        VStack{
+                            DatePicker("", selection: $birthday, displayedComponents: .date)
+                                .offset(x: -100)
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(Color("trending_block_color"))
+                                
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    
+                    Spacer()
+                    HStack{
+                        Text("信箱")
+                            .foregroundStyle(Color("line_color"))
+                            .font(.system(size: 18))
+                        VStack{
+                            TextField(
+                                "電子郵件",
+                                text: $account,
+                                axis: .vertical
+                            )
+                            .focused($isEmailFocus)
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(isEmailFocus ? .black : Color("trending_block_color"))
+                                
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    
+                    Spacer()
+                
+                    HStack{
+                        Text("密碼")
+                            .foregroundStyle(Color("line_color"))
+                            .font(.system(size: 18))
+                        VStack{
+                            HStack{
+                                if isSecured {
+                                    SecureField("8位以上的英文數字", text: $password)
+                                        .focused($isPasswordFocus)
+                                } else {
+                                    TextField("8位以上的英文數字", text: $password)
+                                        .focused($isPasswordFocus)
+                                }
+                                
+                                Button(action: {
+                                    isSecured.toggle()
+                                }) {
+                                    Image(systemName: self.isSecured ? "eye.slash" : "eye")
+                                        .accentColor(Color("line_color"))
+                                }
+                            }
+                            
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(isPasswordFocus ? .black : Color("trending_block_color"))
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink{
+                        
+                    }label: {
+                        Text("註冊")
+                            .foregroundStyle(.white)
+                            .font(.system(size: 19, weight: .regular))
+                            .frame(width: 147, height: 34)
+                            .background(Color("label_color"))
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    .padding(.vertical, 5)
+                }
+                .padding(.horizontal, 50)
+                .padding(.vertical)
+                .frame(height: 360)
+                
+                Spacer()
+                
+            }
+            
+
+        }
+        
+        .overlay(alignment: .center){
+            if isSelectedGender {
+                ZStack {
+                    Button{
+                        isSelectedGender = false
+                    }label: {
+                        Color("line_color")
+                            .ignoresSafeArea()
+                            .opacity(0.7)
+                    }
+                    
+                    VStack{
+                        Text("選擇你的性別")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundStyle(.white)
+                            .padding(.bottom, 40)
+                        
+                        HStack{
+                            Button{
+                                gender = false
+                                isSelectedGender = false
+                            }label: {
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 141)
+                                        .foregroundStyle(Color("background_color 2"))
+                                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                                    Image("female_duck")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 101)
+                                }
+                            }
+                            
+                            Spacer()
+                            Button{
+                                gender = true
+                                isSelectedGender = false
+                            }label: {
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 141)
+                                        .foregroundStyle(Color("background_color 2"))
+                                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                                    Image("male_duck")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 101)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 40)
+                        
+                    }
+                    
+                }
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    SignupView()
+}
